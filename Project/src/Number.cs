2693569edc384aee06bc617f14c1c0f7bc6c0a59
Brandon Colbert <brandoncolbertos@gmail.com>
2020-01-22 17:20:48 -0500

    0.1.3
    -Fixed type not displaying on solve cast errors
    -For numbers, changed base type to float, added operators, and require explicit casts to non floating point types

--- Project/src/Number.cs
+++ Project/src/Number.cs
@@ -3,12 +3,15 @@ using System;
 
 namespace Formulas {
 	/// <summary>Any int, long, float, or double</summary>
-	public struct Number : IComparable, IComparable<int>, IEquatable<int>, IComparable<long>, IEquatable<long>, IComparable<float>, IEquatable<float>, IComparable<double>, IEquatable<double> {
-		double value;
+	public struct Number : IComparable, IComparable<Number>, IEquatable<Number>, IComparable<int>, IEquatable<int>, IComparable<long>, IEquatable<long>, IComparable<float>, IEquatable<float>, IComparable<double>, IEquatable<double> {
+		float value;
 
+		public override bool Equals(object obj) => value.Equals(obj);
+		public override int GetHashCode() => value.GetHashCode();
 		public override string ToString() => value.ToString();
 
 		//Comparison and equality
+		public int CompareTo(object obj) => value.CompareTo(obj);
 		public int CompareTo(int other) => value.CompareTo(other);
 		public bool Equals(int other) => value.Equals(other);
 		public int CompareTo(long other) => value.CompareTo(other);
@@ -17,37 +20,55 @@ namespace Formulas {
 		public bool Equals(float other) => value.Equals(other);
 		public int CompareTo(double other) => value.CompareTo(other);
 		public bool Equals(double other) => value.Equals(other);
-		public int CompareTo(object obj) => value.CompareTo(obj);
+		public int CompareTo(Number other) => value.CompareTo(other.value);
+		public bool Equals(Number other) => value.Equals(other.value);
 
 		//Conversion from
 		public static implicit operator Number(int value) => new Number{value = value};
 		public static implicit operator Number(long value) => new Number{value = value};
 		public static implicit operator Number(float value) => new Number{value = value};
-		public static implicit operator Number(double value) => new Number{value = value};
-		
+		public static implicit operator Number(double value) => new Number{value = (float)value};
+
 		//Conversions into
-		public static implicit operator int(Number number) => (int)number.value;
-		public static implicit operator long(Number number) => (long)number.value;
-		public static implicit operator float(Number number) => (float)number.value;
+		public static explicit operator int(Number number) => (int)number.value;
+		public static explicit operator long(Number number) => (long)number.value;
+		public static implicit operator float(Number number) => number.value;
 		public static implicit operator double(Number number) => number.value;
 
 		//From object
 		public static bool TryParse(object value, out Number number) {
 			switch(value) {
-				case sbyte v: number = v; return true;
-				case byte v: number = v; return true;
-				case short v: number = v; return true;
-				case ushort v: number = v; return true;
-				case int v: number = v; return true;
-				case uint v: number = v; return true;
-				case long v: number = v; return true;
-				case ulong v: number = v; return true;
-				case float v: number = v; return true;
-				case decimal v: number = (double)v; return true;
-				case double v: number = v; return true;
-				case Number v: number = v; return true;
+				case sbyte v: number.value = v; return true;
+				case byte v: number.value = v; return true;
+				case short v: number.value = v; return true;
+				case ushort v: number.value = v; return true;
+				case int v: number.value = v; return true;
+				case uint v: number.value = v; return true;
+				case long v: number.value = v; return true;
+				case ulong v: number.value = v; return true;
+				case float v: number.value = v; return true;
+				case decimal v: number.value = (float)v; return true;
+				case double v: number.value = (float)v; return true;
+				case Number v: number.value = v; return true;
 				default: number = double.NaN; return false;
 			}
 		}
+
+		//Operators
+		public static Number operator +(Number n) => n.value;
+		public static Number operator -(Number n) => -n.value;
+		public static Number operator ++(Number n) => ++n.value;
+		public static Number operator --(Number n) => --n.value;
+		public static Number operator +(Number lhs, Number rhs) => lhs.value + rhs.value;
+		public static Number operator -(Number lhs, Number rhs) => lhs.value - rhs.value;
+		public static Number operator *(Number lhs, Number rhs) => lhs.value * rhs.value;
+		public static Number operator /(Number lhs, Number rhs) => lhs.value / rhs.value;
+		public static Number operator %(Number lhs, Number rhs) => lhs.value % rhs.value;
+		public static bool operator ==(Number lhs, Number rhs) => lhs.value == rhs.value;
+		public static bool operator !=(Number lhs, Number rhs) => lhs.value != rhs.value;
+		public static bool operator <(Number lhs, Number rhs) => lhs.value < rhs.value;
+		public static bool operator >(Number lhs, Number rhs) => lhs.value > rhs.value;
+		public static bool operator >=(Number lhs, Number rhs) => lhs.value >= rhs.value;
+		public static bool operator <=(Number lhs, Number rhs) => lhs.value <= rhs.value;
 	}
 }
